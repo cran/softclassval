f677be53f4ffa58d567e6df7c0b0af1ddf79a6fa
C. Beleites <cbeleites@units.it>
2011-08-04 00:00:00 +0000

    version 0.5-20110804

---
+++ R/hardclasses.R
@@ -0,0 +1,127 @@
+##' Convert to hard class labels
+##'
+##' Converts the soft class labels in \code{x} into a factor with hard class memberships and
+##' \code{NA} for soft samples. 
+##' @param x matrix or array holding the class memberships
+##' @param classdim dimension that holds the classes, default columns
+##' @param soft.name level for soft samples 
+##' @param tol tolerance: samples with membership >= 1 - tol are considered to be hard samples of the
+##' respective class.
+##' @param drop see \code{\link[arrayhelpers]{drop1d}}
+##' @return factor array of shape \code{dim (x) [-classdim]}
+##' @author Claudia Beleites
+##' @export 
+hardclasses <- function (x, classdim = 2L, soft.name = NA, tol = 1e-5, drop = TRUE){
+  if (ndim (x) == 0) {                 # vector
+    warning ("Using hardclasses (cbind (x, 1 - x)) instead.")
+    x <- cbind (x, 1 - x)
+    colnames (x) <- 1 : 0
+  }
+  
+  classdim <- numericindex (x = dim (x), i = classdim, n = names (dimnames (x)))
+  x <- aperm (x, c(seq_len (ndim (x))[-classdim], classdim))
+  x <- makeNd (x, -2)
+  olddims <- attr (x, "old")[[1]]
+  
+  if (any (abs(1 - rowSums (x)) > tol))
+    warning ("Found samples with total membership != 1")
+  
+  if (is.null (classes <- colnames (x)))
+      classes <- paste ("class", seq_len (ncol (x)), sep = "")
+  
+  x <- x >= 1 - tol                     # looses attributes!
+
+  cl <- apply (x, 1, function (x) match (TRUE, x))
+  
+  if (! is.na (soft.name)){
+    classes <- c (classes, soft.name)
+    cl [is.na (cl)] <- length (classes)
+  }
+
+  cl <- structure (cl,
+                   .Label    = classes, class = "factor",
+                   .Dim      =      head (olddims$dim,      -1),
+                   .Dimnames = lon (head (olddims$dimnames, -1)))
+  drop1d (cl, drop = drop)
+}
+
+test (hardclasses) <- function (){
+  checkEquals (hardclasses (pred),
+               factor (rep (letters [c (1, 2, NA, NA, NA)], 2), levels = letters [1 : 3]))
+
+  checkEquals (hardclasses (pred, drop = FALSE), ensuredim (hardclasses (pred)))
+
+  tmp <- pred
+  dim (tmp) <- c (5, 2, 3)
+  checkEquals (hardclasses (tmp, 3),
+         structure (c (1L, 2L, NA, NA, NA, 1L, 2L, NA, NA, NA), .Dim = c(5L, 2L),
+                    .Label = c("class1", "class2", "class3"), class = "factor"))
+  
+  ## vectors
+  warn <- options(warn = 2)$warn
+  on.exit (options (warn = warn))
+  checkException (hardclasses (pred [,1]))
+  options(warn = 1)
+  checkEquals (hardclasses (pred [, 1]),
+               factor (rep (c ("1", "0", NA, NA, NA), 2), levels = c ("1", "0")))
+}
+
+## helper for hard "and" operator: sets all values NA that are not within +- tol from 0 or 1.
+.make01 <- function (x, tol = 1e-6){
+  tmp <- rep (NA_real_, length (x))
+  tmp [x >    -tol & x <     tol] <- 0
+  tmp [x > 1 - tol & x < 1 + tol] <- 1
+  
+  attributes (tmp) <- attributes (x)
+
+  tmp
+}
+test (.make01) <- function (){
+  checkIdentical (.make01 (v), c( a = 0, b = NA, c = NA, d = 1, e = NA))
+  checkIdentical (attributes (.make01 (m)), attributes (m))
+}
+
+##' Mark operator as hard measure
+##'
+##' The operators may work only for hard classes (see \code{\link[softclassval:operators]{and}}). \code{hard (op)
+##' == TRUE} marks hard operators.
+##' 
+##' @param op the operator (function)
+##' @return logical indicating the type of operator. \code{NULL} if the attribute is missing.
+##' @author Claudia Beleites
+##' @seealso \code{\link{sens}} \code{\link[softclassval:operators]{and}}
+##' @export 
+##' @include softclassval.R
+##'
+##' @examples
+##'
+##' hard (and)
+##' myop <- function (r, p) p * (r == 1)
+##' hard (myop) <- TRUE
+##' 
+
+hard <- function (op)
+  attr (op, "hard")
+
+##' @usage hard (op) <- value
+##' @rdname hard
+##' @param value logical indicating the operator type
+##' @export "hard<-"
+"hard<-" <- function (op, value){
+  stopifnot (is.logical (value), !is.na (value))
+
+  attr (op, "hard") <- value
+
+  op
+}
+
+test (hard) <- function (){
+  myop <- function (){}
+  checkTrue (is.null (hard (myop)))
+  hard (myop) <- TRUE
+  checkTrue (hard (myop))
+  hard (myop) <- FALSE
+  checkTrue (!hard (myop))
+  checkException (hard (myop) <- NULL)
+  checkException (hard (myop) <- NA)
+}
---
+++ R/operators.R
@@ -0,0 +1,226 @@
+##' @encoding UTF8
+##' @description And (conjunction) operators
+##'
+##' And operators for the soft performance calculation.
+##'
+##' The predefined operators are:
+##' \tabular{llllll}{
+##' Name         \tab Definition                 \tab \code{\link{dev}}? \tab \code{\link{postproc}}?  \tab \code{\link{hard}}? \tab Explanation                                                           \cr
+##' \code{gdl}   \tab \code{pmin (r, p)}         \tab FALSE              \tab FALSE                    \tab FALSE               \tab the \enc{Gödel}{Goedel}-operator (weak conjunction)                   \cr
+##' \code{luk}   \tab \code{pmax (r + p - 1, 0)} \tab FALSE              \tab FALSE                    \tab FALSE               \tab \enc{Łukasiewicz}{Lukasiewicz}-operator (strong conjunction)          \cr
+##' \code{prd}   \tab \code{r * p}               \tab FALSE              \tab FALSE                    \tab FALSE               \tab product operator                                                      \cr
+##' \code{and}   \tab \code{r * p}               \tab FALSE              \tab FALSE                    \tab TRUE                \tab Boolean conjunction: accepts only 0 or 1, otherwise yields \code{NA}  \cr
+##' \code{wMAE}  \tab \code{r * abs (r - p)}     \tab TRUE               \tab FALSE                    \tab FALSE               \tab for weighted mean absolute error                                      \cr
+##' \code{wMSE}  \tab \code{r * (r - p)^2}       \tab TRUE               \tab FALSE                    \tab FALSE               \tab for weighted mean squared error                                       \cr
+##' \code{wRMSE} \tab \code{r * (r - p)^2}       \tab TRUE               \tab TRUE                     \tab FALSE               \tab for root weighted mean squared error                                  \cr
+##' }
+##'
+##' @param p prediction vector, matrix, or array with numeric values in [0, 1], for \code{and} in \{0, 1\}
+##' @param r reference vector, matrix, or array with numeric values in [0, 1], for \code{and} in \{0, 1\}
+##' @return numeric of the same size as p
+##' @author Claudia Beleites
+##' @seealso Performance measures: \code{\link{sens}}
+##' @references see the literature in \code{citation ("softclassval")}
+##' @rdname operators
+##' @export
+##' @include softclassval.R
+##' @include dev.R
+##' @include postproc.R
+##'
+##' @examples
+##' ops <- c ("luk", "gdl", "prd", "and", "wMAE", "wMSE", "wRMSE")
+##' 
+##' ## make a nice table
+##'
+##' 
+##' lastline <- function (f){
+##'   body <- deparse (body (get (f))) ## function body
+##'   body <- body [!grepl ("^[ \t]*[}][ \t]*$", body)]
+##'   gsub ("^[ \t]+([^ \t].*[^ \t])[ \t]*$", "\\1", tail (body, 1))
+##' }
+##' 
+##' data.frame (source = sapply (ops, lastline),
+##'             dev = sapply (ops, function (f) dev (get (f))),
+##'             hard = sapply (ops, function (f) hard (get (f))),
+##'             postproc = I (lapply (ops, function (f) postproc (get (f))))
+##'             )
+##' 
+##' x <- softclassval:::v
+##' x
+##' 
+##' luk (0.7, 0.8)
+##' 
+##' ## The behaviour of the operators
+##' ## op (x, 1)
+##' cbind (x, sapply (c ("luk", "gdl", "prd", "wMAE", "wMSE", "wRMSE"),
+##'                   function (op, x) get (op) (x, 1), x))
+##' 
+##' ## op (x, 0)
+##' cbind (x, sapply (c ("luk", "gdl", "prd", "wMAE", "wMSE", "wRMSE"),
+##'                   function (op, x) get (op) (x, 0), x))
+##' 
+##' ## op (x, x)
+##' cbind (x, sapply (c ("luk", "gdl", "prd", "wMAE", "wMSE", "wRMSE"),
+##'                   function (op, x) get (op) (x, x), x))
+##' 
+##' 
+##' ## Note that the deviation operators are not commutative
+##' ## (due to the weighting by reference)
+##' zapsmall (
+##' cbind (sapply (c ("luk", "gdl", "prd", "wMAE", "wMSE", "wRMSE"),
+##'                   function (op, x) get (op) (1, x), x)) -
+##' cbind (sapply (c ("luk", "gdl", "prd", "wMAE", "wMSE", "wRMSE"),
+##'                   function (op, x) get (op) (x, 1), x)) 
+##' )
+##' 
+##' 
+luk <- function (r, p)
+  pmax (r + p - 1, 0)
+dev (luk) <- FALSE
+hard (luk) <- FALSE
+
+test (luk) <- function(){
+  checkEqualsNumeric (luk (v, v),       c (a = 0,  b = 0,   c = 0.4, d = 1,   e = NA))
+  checkEqualsNumeric (luk (v, rev (v)), c (a = NA, b = 0.3, c = 0.4, d = 0.3, e = NA))
+}
+
+##' @rdname operators
+##' @export 
+gdl <- function (r, p)
+  pmin (p, r)                           # Note: takes attributes from p only
+dev (gdl) <- FALSE
+hard (gdl) <- FALSE
+
+test (gdl) <- function(){
+  checkEqualsNumeric (gdl (v, v),       v)
+  checkEqualsNumeric (gdl (v, rev (v)), c (a = NA, b = 0.3, c = 0.7, d = 0.3, e = NA))
+}
+
+##' @rdname operators
+##' @export 
+prd <- function (r, p)  r * p
+dev (prd) <- FALSE
+hard (prd) <- FALSE
+
+test (prd) <- function(){
+  checkEqualsNumeric (prd (v, v),       v^2)
+  checkEqualsNumeric (prd (v, rev (v)), c (a = NA, b = 0.3, c = 0.49, d = 0.3, e = NA))
+}
+
+##' @rdname operators
+##' @export 
+and <- function (r, p){ # the boolean and: accepts only hard r and p
+  mostattributes (r) <- attributes (p)  
+  p <- .make01 (p)
+  r <- .make01 (r)
+    
+  r * p ## fastest
+}
+dev (and) <- FALSE
+hard (and) <- TRUE
+
+test (and) <- function(){
+  checkEqualsNumeric (and (v, v),       c (a = 0 , b = NA, c = NA, d =  1, e = NA))
+  checkEqualsNumeric (and (v, rev (v)), c (a = NA_real_, b = NA, c = NA, d = NA, e = NA))
+  checkEqualsNumeric (and (0, 1), 0)
+}
+
+##' @rdname operators
+##' @export 
+wMAE <- function (r, p) {
+  mostattributes (r) <- attributes (p)
+  r * abs (p - r)
+}
+dev (wMAE) <- TRUE
+hard (wMAE) <- FALSE
+
+test (wMAE) <- function(){
+  checkEqualsNumeric (wMAE (v, v),       c (a = 0 , b = 0,    c = 0, d = 0,   e = NA))
+  checkEqualsNumeric (wMAE (v, rev (v)), c (a = NA, b = 0.21, c = 0, d = 0.7, e = NA))
+}
+
+
+##' @rdname operators
+##' @export 
+wMSE <- function (r, p)
+  r * (p - r)^2
+
+dev (wMSE) <- TRUE
+hard (wMSE) <- FALSE
+
+##' @rdname operators
+##' @export 
+wRMSE <- wMSE
+postproc (wRMSE) <- "sqrt"
+
+
+##' @nord
+testoperators <- svTest (function (){
+  ops <- c ("luk", "gdl", "prd", "and", "wMAE", "wMSE", "wRMSE")
+
+  ## dev
+  for (o in setdiff (ops, c ("wMAE", "wMSE", "wRMSE")))
+    checkTrue (! dev (get (o)),
+               msg = sprintf ("dev: %s", o))
+  for (o in c ("wMAE", "wMSE", "wRMSE"))
+    checkTrue (dev (get (o)),
+               msg = sprintf ("dev: %s", o))
+  ## hard
+  for (o in setdiff (ops, "and"))
+    checkTrue (! hard (get (o)),
+               msg = sprintf ("hard: %s", o))
+  checkTrue (hard (and),
+             msg = sprintf ("hard: and"))
+
+  ## postproc
+  for (o in setdiff (ops, "wRMSE"))
+    checkTrue (is.null (postproc (get (o))),
+               msg = sprintf ("postproc: %s", o))
+  checkEquals (postproc (wRMSE), "sqrt")
+
+  ## against 1
+  checkEquals (sapply (ops, function (x) get (x) (1, v)),
+               matrix (c (0,   0,   0,   0,   1,   1,    1,
+                          0.3, 0.3, 0.3, NA,  0.7, 0.49, 0.49, 
+                          0.7, 0.7, 0.7, NA,  0.3, 0.09, 0.09,
+                          1,   1,   1,   1,   0,   0,    0,
+                          NA,  NA,  NA,  NA,  NA,  NA,   NA), byrow = TRUE, nrow = 5,
+                       dimnames = list (names (v), ops))
+               )
+
+  ## against 0
+  checkEquals (sapply (ops, function (x) get (x) (0, v)),
+               matrix (c (0,   0,   0,   0,   0,   0,    0,
+                          0,   0,   0,   NA,  0,   0,    0,
+                          0,   0,   0,   NA,  0,   0,    0,
+                          0,   0,   0,   0,   0,   0,    0,
+                          NA,  NA,  NA,  NA,  NA,  NA,   NA), byrow = TRUE, nrow = 5,
+                       dimnames = list (names (v), ops))
+               )
+
+  r <- runif (1000)
+  p <- runif (1000)
+  for (o in c ("luk", "gdl", "prd", "wMAE", "wMSE", "wRMSE"))
+    checkTrue (get (o) (r, p) <= r, msg = sprintf ("op (p, r) <= r: %s", o))
+
+  for (o in ops){
+    op <- get (o)
+    checkEqualAttributes (op (runif (nrow (m)), makeNd (m, 3)),
+                          makeNd (m, 3),
+                          msg = sprintf ("preserve shape of p: %s", o))
+
+    checkEqualsNumeric (op (m [, 1], m),
+                        op (m [, rep (1, ncol(m))], m),
+                        msg = sprintf ("recycling: %s", o))
+  }
+  
+  tmp <- runif (length (m))
+  mostattributes (tmp) <- attributes (m)
+  for (o in c ("luk", "gdl", "prd", "and")){
+    op <- get (o)
+    checkEquals (op (m, tmp),
+                 op (tmp, m),
+                 msg = sprintf ("commutativity: %s", o))
+  }
+  
+})
